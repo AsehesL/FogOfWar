f0ea7a485b12445beb96c7eaad0de109ddced82c
gitsen <342922630@qq.com>
2017-10-30 23:43:34 +0800

    地图初始化脚本

--- Assets/Scripts/FogOfWar/Core/FOWMap.cs
+++ Assets/Scripts/FogOfWar/Core/FOWMap.cs
@@ -6,13 +6,42 @@ namespace ASL.FogOfWar
 {
     internal class FOWMap
     {
-        public int[,] mapData { get { return m_MapData; } }
+        public byte[,] mapData { get { return m_MapData; } }
 
-        private int[,] m_MapData;
+        private byte[,] m_MapData;
 
         public FOWMap()
         {
 
+        }
+
+        public void GenerateMap(Vector3 position, float xSize, float zSize, int texWidth,
+            int texHeight,
+            float heightRange)
+        {
+            m_MapData = new byte[texWidth, texHeight];
+
+            float deltax = xSize / texWidth;
+            float deltay = zSize / texHeight;
+            Vector3 origin = position - new Vector3(xSize / 2, 0, zSize / 2);
+
+            for (int i = 0; i < texWidth; i++)
+            {
+                for (int j = 0; j < texHeight; j++)
+                {
+                    float x = origin.x + i * deltax + deltax / 2;
+                    float y = origin.y + j * deltay + deltay / 2;
+                    Ray ray = new Ray(new Vector3(x, origin.y + heightRange, y), Vector3.down);
+                    if (Physics.Raycast(ray, heightRange))
+                    {
+                        m_MapData[i, j] = 1;
+                    }
+                    else
+                    {
+                        m_MapData[i, j] = 0;
+                    }
+                }
+            }
         }
 
     }
